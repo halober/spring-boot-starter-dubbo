51ad61dec114aff2de41ef0c19c61c7e80d0f0ba
雷 <619137943@qq.com>
2017-09-13 01:46:36 +0800

    "

--- spring-boot-starter-dubbo/install.cmd
+++ spring-boot-starter-dubbo/install.cmd
@@ -1,1 +1,1 @@
-start mvn clean source:jar jar:jar javadoc:jar install -Dmaven.test.skip=true -Darguments="gpg.passphrase=lei0719"
+start mvn clean source:jar jar:jar javadoc:jar install -Dmaven.test.skip=true
