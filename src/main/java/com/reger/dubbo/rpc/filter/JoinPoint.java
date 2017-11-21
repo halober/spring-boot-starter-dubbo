b3cd0c9a10073d51eca4bcf62fba9b77f6fc1e30
雷 <619137943@qq.com>
2017-11-21 15:49:51 +0800

    ”

--- src/main/java/com/reger/dubbo/rpc/filter/JoinPoint.java
+++ src/main/java/com/reger/dubbo/rpc/filter/JoinPoint.java
@@ -1,10 +1,11 @@
 package com.reger.dubbo.rpc.filter;
 
+import java.util.Map;
+
 import com.alibaba.dubbo.common.Node;
-import com.alibaba.dubbo.rpc.Invocation;
 import com.alibaba.dubbo.rpc.Result;
 
-public interface JoinPoint<T> extends Node, Invocation {
+public interface JoinPoint<T> extends Node {
 
 	/**
 	 * 调用真实方法
@@ -17,4 +18,43 @@ public interface JoinPoint<T> extends Node, Invocation {
 	 * 接口信息
 	 */
 	Class<T> getInterface();
+	
+    /**
+     * 方法名
+     * @return
+     */
+    String getMethodName();
+
+    /**
+     * 参数类型
+     * @return
+     */
+    Class<?>[] getParameterTypes();
+
+    /**
+     * 参数名
+     * @return
+     */
+    Object[] getArguments();
+
+    /**
+     * 隐式传参
+     * @return
+     */
+    Map<String, String> getAttachments();
+
+    /**
+     * 获取隐式参数
+     * @param key 参数名
+     * @return
+     */
+    String getAttachment(String key);
+
+    /**
+     * 获取隐式参数
+     * @param key 参数名
+     * @param defaultValue 参数默认值
+     * @return 
+     */
+    String getAttachment(String key, String defaultValue);
 }
