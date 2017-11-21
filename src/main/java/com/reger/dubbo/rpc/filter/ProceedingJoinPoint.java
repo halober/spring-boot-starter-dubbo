b050490ce6befc06a2c00da8bc597413427ffeae
雷 <619137943@qq.com>
2017-11-21 14:02:27 +0800

    添加日志

--- src/main/java/com/reger/dubbo/rpc/filter/ProceedingJoinPoint.java
+++ src/main/java/com/reger/dubbo/rpc/filter/ProceedingJoinPoint.java
@@ -2,11 +2,17 @@ package com.reger.dubbo.rpc.filter;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.alibaba.dubbo.rpc.Invocation;
 import com.alibaba.dubbo.rpc.Invoker;
 import com.alibaba.dubbo.rpc.Result;
 
 public class ProceedingJoinPoint {
+	
+	private static final Logger log = LoggerFactory.getLogger(ProceedingJoinPoint.class);
+
 	private final Invoker<?> invoker;
 	private final Invocation invocation;
 	private final List<? extends RpcFilter> rpcFilters;
@@ -24,6 +30,7 @@ public class ProceedingJoinPoint {
 	}
 
 	public Result proceed() {
+		log.debug("注册了{}个过滤器，当前调用第{}个,过滤器通过beanName排序",rpcFilterSize,index);
 		if (index >= rpcFilterSize) {
 			return invoker.invoke(invocation);
 		} else {
